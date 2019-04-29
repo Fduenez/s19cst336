a6db73f6d88d4df9655431d9942183aa2e299ba8
Frank Duenez <fduenez@csumb.edu>
2019-04-29 01:31:48 +0000

    added heart.php and unheart.php

--- Labs/lab8/favorite.php
+++ Labs/lab8/favorite.php
@@ -28,6 +28,7 @@
       
       <label>search images:</label><input type="text" id="query">
       <button type = button id = "button">submit</button>
+      <button type = button id = "favorite">all favorites</button>
       <div id="container"> </div>
         
     </fieldset>
@@ -35,7 +36,7 @@
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>
-  
+                /* global $*/
                 $("#button").on("click", function(e)
                 {
                   $("#container").empty();
@@ -67,8 +68,12 @@
                             img.setAttribute("src", data[i+j]['largeImageURL']);
                             
                             var heart = document.createElement("img");
-                            heart.setAttribute("src","favorite-on.png");
+                            
+                            heart.setAttribute("src","unfavorite.png");
+                            heart.setAttribute("id", data[i+j]['largeImageURL']);
                             heart.setAttribute("class","heart");
+                            heart.setAttribute("onclick", "changeHeart(this.id)");
+                            
                             
                             d.appendChild(img);
                             d.appendChild(heart);
@@ -84,6 +89,106 @@
                    } 
                   });
                 });
+                
+                function changeHeart(pic)
+                {
+                  if(document.getElementById(pic).getAttribute("src") == "unfavorite.png")
+                  {
+                    document.getElementById(pic).setAttribute("src", "favorite-on.png")
+                  
+                  
+                    $.ajax
+                    ({
+                      type:"GET",
+                      url: "api/heart.php",
+                      dataType:"json",
+                      data:
+                      {
+                        "url": pic,
+                      },
+                      success:function(data, status)
+                      {
+                        console.log(data);
+                      } ,
+                     complete: function(status, err){
+                       console.log(status);
+                     } 
+                    });
+                  }
+                  else
+                  {
+                    document.getElementById(pic).setAttribute("src", "unfavorite.png")
+                    
+                    $.ajax
+                    ({
+                      type:"GET",
+                      url: "api/unheart.php",
+                      dataType:"json",
+                      data:
+                      {
+                        "url": pic,
+                      },
+                      success:function(data, status)
+                      {
+                        console.log(data);
+                      } ,
+                    complete: function(status, err){
+                      console.log(status);
+                    } 
+                    });
+                    
+                  }
+                  
+                }
+                
+          $( "#favorite" ).click(function() {
+            
+            $( "#container" ).empty();
+            
+            $.ajax({
+                type: "GET",
+                url: "api/allFavorites.php",
+                dataType: "json",
+                data:
+                {
+                  
+                },
+                success: function(data) {
+                    console.log(data);
+                    
+                    
+                    // for(var i=0; i < data.length; i += 3){
+                    //     var col = document.createElement("DIV");
+                    //     col.setAttribute("class", "col");
+                        
+                    //     for(var j=0; j<3 && i+j < data.length; j++){
+                        
+                    //         var imgdiv = document.createElement("DIV");
+                    //         imgdiv.setAttribute("class", "imgdiv");
+                            
+                    //         var image = document.createElement("IMG");
+                    //         image.setAttribute("src", data[i+j]["link"]);
+                    //         imgdiv.appendChild(image);
+                            
+                    //         var heart = document.createElement("IMG"); // < img >
+                    //         heart.setAttribute("src", "img/favorite-on.png"); // < img src="img/favorite.png">
+                    //         heart.setAttribute("class", "heart"); // < img src="img/favorite.png" class="heart">
+                    //         heart.setAttribute("onclick", "heartClicked(this.id)")
+                    //         heart.setAttribute("id", data[i+j]["link"]);
+                    //         imgdiv.appendChild(heart);
+                            
+                    //         col.appendChild(imgdiv);
+                        // }
+                        
+                        // document.getElementById("images").appendChild(col);
+                    // }
+                     
+                },
+                error: function(status,err) {
+                    console.log(arguments);  
+                },
+            });
+        });
   </script>
 </body>
 
