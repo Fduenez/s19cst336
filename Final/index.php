5f55fb4d4ea7b86a61cb717f6dda5b89a8f3c5fe
Frank Duenez <fduenez@csumb.edu>
2019-05-13 05:50:09 +0000

    worked

--- Final/index.php
+++ Final/index.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!--<link rel="stylesheet" href="css/index.css" type="text/css" />-->
+        <link rel="stylesheet" href="css/index.css" type="text/css" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
@@ -186,9 +186,9 @@
         {
             console.log(i);
             identification = i;
-            $('.modalRemove').modal({
-              keyboard: false
-            })
+            // $('.modalRemove').modal({
+            //   keyboard: false
+            // })
             //  $.ajax({
             //     type: "POST",
             //     url: "api/deleteRow.php",
